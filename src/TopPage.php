15b4b96216e1eb66b9daa3a8ddbc718b4eb2616e
ponponumi <ponponumi@ponponumi.com>
2024-12-28 18:18:28 +0900

    トップページが「最新の投稿」ならtrue、「固定ページ」ならfalseを返すメソッドを作成

--- src/TopPage.php
+++ src/TopPage.php
@@ -4,5 +4,17 @@ namespace Ponponumi\PonponcatDataget;
 
 class TopPage
 {
-    //
+    public static function isNewPost(): bool
+    {
+        // トップページが「最新の投稿」ならtrue、「固定ページ」ならfalseを返す
+        $topOption = get_option("show_on_front");
+
+        if($topOption === "posts"){
+            // ホームページが最新の投稿なら
+            return true;
+        }else{
+            // ホームページが固定ページなら
+            return false;
+        }
+    }
 }
