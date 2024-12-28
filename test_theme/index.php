b7fb093e0a74dce636335e92546bb1fcc03644e9
ponponumi <ponponumi@ponponumi.com>
2024-12-28 18:29:26 +0900

    表を作成

--- test_theme/index.php
+++ test_theme/index.php
@@ -6,6 +6,16 @@
     <title>テスト</title>
 </head>
 <body>
-    <!--  -->
+    <table>
+        <thead>
+            <tr>
+                <th>データの内容</th>
+                <th>現在の設定</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!--  -->
+        </tbody>
+    </table>
 </body>
 </html>
