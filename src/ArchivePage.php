a3347dbd292ca3c413f3be7ebc241957b7f3aeb5
ponponumi <ponponumi@ponponumi.com>
2024-12-28 19:06:35 +0900

    現在の投稿タイプのURLを取得するメソッドを作成

--- src/ArchivePage.php
+++ src/ArchivePage.php
@@ -4,5 +4,19 @@ namespace Ponponumi\PonponcatDataget;
 
 class ArchivePage
 {
-    //
+    public static function postTypeGet(): string
+    {
+        // 現在の投稿タイプのURLを取得
+        $postType = "";
+
+        if (is_singular()) {
+            // 記事ページの場合
+            $postType = get_post_type();
+        } elseif (is_post_type_archive()) {
+            // アーカイブページの場合
+            $postType = get_query_var("post_type");
+        }
+
+        return $postType;
+    }
 }
