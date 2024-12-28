90f5c6db4b7bc9d9de302317c09a3f33e7269cc5
ponponumi <ponponumi@gmail.com>
2024-12-29 01:00:27 +0900

    フッターをパーツ化

---
+++ test_theme/footer.php
@@ -0,0 +1,25 @@
+    </div>
+    <table>
+        <thead>
+            <tr>
+                <th>データの内容</th>
+                <th>現在の設定</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>トップページが「最新の投稿」かどうか</td>
+                <td><?php var_dump(\Ponponumi\PonponcatPostDataget\TopPage::settingIsNewPost()) ?></td>
+            </tr>
+            <tr>
+                <td>トップページが「固定ページ」かどうか</td>
+                <td><?php var_dump(\Ponponumi\PonponcatPostDataget\TopPage::settingIsFixedPage()) ?></td>
+            </tr>
+            <tr>
+                <td>ブログのトップページのURL</td>
+                <td><?php var_dump(\Ponponumi\PonponcatPostDataget\TopPage::postTopPageUrlGet()) ?></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
