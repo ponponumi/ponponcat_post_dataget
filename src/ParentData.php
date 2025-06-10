d02151739fcde9c3473b623a19a2b8aa4146f9c1
ponponumi <ponponumi@ponponumi.com>
2025-06-10 17:46:02 +0900

    現在のカテゴリーを追加するかどうか、選べるように変更

--- src/ParentData.php
+++ src/ParentData.php
@@ -6,20 +6,23 @@ use WP_Term;
 
 class ParentData
 {
-    public static function parentCategoriesGet(WP_Term $categoryObject): array
+    public static function parentCategoriesGet(WP_Term $categoryObject, bool $nowCategoryAdd=true): array
     {
         // 親カテゴリーを取得
         $result = [];
 
-        while(true){
+        if($nowCategoryAdd){
             $result[] = $categoryObject;
+        }
 
+        while(true){
             if($categoryObject->parent === 0){
                 // 親カテゴリーがなくなったら
                 break;
             }
 
             $categoryObject = get_category($categoryObject->parent);
+            $result[] = $categoryObject;
         }
 
         $result = array_reverse($result);
