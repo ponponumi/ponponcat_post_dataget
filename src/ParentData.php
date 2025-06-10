385aee5a815ede40bacb4c7218ad71ea1fc0cf17
ponponumi <ponponumi@ponponumi.com>
2025-06-10 17:43:47 +0900

    親カテゴリーを取得するメソッドを作成

--- src/ParentData.php
+++ src/ParentData.php
@@ -2,7 +2,28 @@
 
 namespace Ponponumi\PonponcatPostDataget;
 
+use WP_Term;
+
 class ParentData
 {
-    //
+    public static function parentCategoriesGet(WP_Term $categoryObject): array
+    {
+        // 親カテゴリーを取得
+        $result = [];
+
+        while(true){
+            $result[] = $categoryObject;
+
+            if($categoryObject->parent === 0){
+                // 親カテゴリーがなくなったら
+                break;
+            }
+
+            $categoryObject = get_category($categoryObject->parent);
+        }
+
+        $result = array_reverse($result);
+
+        return $result;
+    }
 }
