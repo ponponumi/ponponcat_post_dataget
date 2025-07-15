cb5cb48f8b5f93c45362c9c0d10625003eae8a68
ponponumi <ponponumi@gmail.com>
2025-07-15 18:26:00 +0900

    ワードを取得して返すメソッドを作成

--- src/SearchResult.php
+++ src/SearchResult.php
@@ -4,5 +4,15 @@ namespace Ponponumi\PonponcatPostDataget;
 
 class SearchResult
 {
-    //
+    public static function word(bool $escMode=true): string
+    {
+        $word = get_search_query(false);
+
+        if($escMode){
+            // エスケープする場合
+            $word = htmlspecialchars($word, ENT_QUOTES);
+        }
+
+        return $word;
+    }
 }
