185fa854d5010c723ed23c9a6e353fa1085b90ef
ponponumi <ponponumi@gmail.com>
2025-07-19 21:08:51 +0900

    表を作成

--- test_theme/search.php
+++ test_theme/search.php
@@ -2,4 +2,16 @@
 
 <h1>検索結果</h1>
 
+<table>
+    <thead>
+        <tr>
+            <th>データの内容</th>
+            <th>取得結果</th>
+        </tr>
+    </thead>
+    <tbody>
+        <!--  -->
+    </tbody>
+</table>
+
 <?php get_footer() ?>
