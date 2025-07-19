e4737d1096cf0764b76578a6af713f6c7e97630b
ponponumi <ponponumi@gmail.com>
2025-07-19 21:22:18 +0900

    wordGetメソッドの説明を追加

--- src/SearchResult.php
+++ src/SearchResult.php
@@ -4,6 +4,12 @@ namespace Ponponumi\PonponcatPostDataget;
 
 class SearchResult
 {
+    /**
+     * 検索ワードを取得します。検索ページ以外では、空の文字列を返します。
+     *
+     * @param bool $escMode htmlspecialchars関数で、エスケープするかどうかを選びます。
+     * @return string
+     */
     public static function wordGet(bool $escMode=true): string
     {
         if(!is_search()){
