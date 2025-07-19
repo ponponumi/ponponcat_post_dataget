1108636bcbdbeee976fbd0d9083a0b0c44273c3a
ponponumi <ponponumi@gmail.com>
2025-07-19 20:58:31 +0900

    エスケープしないテストを追加

--- test_theme/footer.php
+++ test_theme/footer.php
@@ -176,6 +176,17 @@
                     )) ?></pre>
                 </td>
             </tr>
+            <tr>
+                <td>現在のページの親の時期別アーカイブ(日まで、ここではエスケープしないテスト)</td>
+                <td>
+                    <pre><?php var_dump(\Ponponumi\PonponcatPostDataget\ParentData::nowPageDateArchiveGet(
+                        howFar: "d",
+                        yearFormat: "Y年>>",
+                        monthFormat: "Y年n月>>",
+                        dayFormat: "Y年n月j日>>"
+                    )) ?></pre>
+                </td>
+            </tr>
         </tbody>
     </table>
 </body>
